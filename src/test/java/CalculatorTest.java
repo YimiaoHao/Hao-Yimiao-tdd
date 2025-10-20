5f523181f62d030454abdf25dcc24efecb86ea17
YimiaoHao <Haoyimiao0407@outlook.com>
2025-10-20 23:41:25 +0800

    test: multiply_twoTimesFour_returnsEight

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class CalculatorTest {
+class CalculatorTest  {
     @Test
     void add_twoPlusThree_returnsFive() {
         Calculator c = new Calculator();
@@ -11,8 +11,21 @@ class CalculatorTest {
 
     @Test
     void add_onePlusOne_returnsTwo() {
-        var c = new Calculator();
+        Calculator c = new Calculator();
         assertEquals(2, c.add(1, 1));
     }
+
+    @Test
+    void subtract_threeMinusOne_returnsTwo() {
+        Calculator c = new Calculator();
+        assertEquals(2, c.subtract(3, 1));
+    }
+
+    @Test
+    void multiply_twoTimesFour_returnsEight() {
+        Calculator c = new Calculator();
+        assertEquals(8, c.multiply(2, 4));
+}
+
 
 }
